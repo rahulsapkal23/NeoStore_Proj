bb190f945cd1ceb24263a6de564b3d3f05bba292
aniket-pracheta <aniket.pracheta@neosofttech.com>
2017-06-27 18:10:51 +0530

    user-account module created and tested its API and validation added

--- common/models/user-account.js
+++ common/models/user-account.js
@@ -2,4 +2,7 @@
 
 module.exports = function(Useraccount) {
 
+  Useraccount.validatesInclusionOf('gender', {in: ['male', 'female'],message:{in:'In valid gender'}});
+  Useraccount.validatesInclusionOf('is_active', {in: [true, false],message:{in:'Only True and false are allowed'}});
+
 };
