b92486190b51193e6de270274cca979a4d812e34
aniket-pracheta <aniket.pracheta@neosofttech.com>
2017-07-10 11:45:48 +0530

    commit as changes in user-account

--- common/models/user-account.js
+++ common/models/user-account.js
@@ -15,9 +15,9 @@ module.exports = function(Useraccount) {
   Useraccount.validatesInclusionOf('gender', {in: ['male', 'female'], message: 'In valid gender'});
   Useraccount.validatesInclusionOf('is_active', {in: [true, false], message: 'Only True and false are allowed'});
 
-  Useraccount.definition.properties.created_date.default = function() {
-    return new Date();
-  }
+  // Useraccount.definition.properties.created_date.default = function() {
+  //   return new Date();
+  // }
 
   Useraccount.disableRemoteMethodByName('prototype.__count__accessTokens');
   Useraccount.disableRemoteMethodByName('prototype.__create__accessTokens');
@@ -27,8 +27,17 @@ module.exports = function(Useraccount) {
   Useraccount.disableRemoteMethodByName('prototype.__get__accessTokens');
   Useraccount.disableRemoteMethodByName('prototype.__updateById__accessTokens');
 
-  //Useraccount.disableRemoteMethodByName('prototype.__create__accessTokens');
-  //Useraccount.disableRemoteMethodByName('prototype.__delete__accessTokens');
+  Useraccount.beforeRemote('create', function(context, user, next) {
+
+    context.args.data.created_date=new Date();
+
+  //context.args.data.publisherId = context.req.accessToken.userId;
+  Useraccount.validatesPresenceOf('password', {message: 'Cannot be blank'});
+
+  console.log(JSON.stringify(user));
+  console.log("-->"+JSON.stringify((context.args.data)));
+  next();
+});
 
   Useraccount.remoteMethod('AdminLogin', {
     accepts: [
@@ -40,6 +49,22 @@ module.exports = function(Useraccount) {
     ],
     http: {path: '/loginAdmin', verb: 'post'}
   });
+
+  Useraccount.remoteMethod('updateFromAdmin', {
+    accepts: [
+      {arg: 'user_id', type: 'string', require: true},
+      {arg: 'first_name', type: 'string', require: true},
+      {arg: 'last_name', type: 'string', require: true},
+      {arg: 'role', type: 'string', require: true},
+      {arg: 'phone_no', type: 'string', require: true},
+      {arg: 'birth_date', type: 'string', require: true}
+    ],
+    returns: [
+      {arg: 'response', type: 'object'}
+    ],
+    http: {path: '/Update_user', verb: 'PUT'}
+  });
+
 
 
   Useraccount.AdminLogin = function (email, password, cb) {
@@ -88,6 +113,24 @@ module.exports = function(Useraccount) {
          }
 
       }
+    });
+  }
+
+  Useraccount.updateFromAdmin=function (user_id,first_name,last_name,role,phone_no,birth_date,cb) {
+    console.log("-->" + user_id+""+last_name+""+role+""+phone_no+""+birth_date);
+
+    //let data={};
+    //Useraccount.updateAttributes(data, callback)
+
+    Useraccount.find({where: {"id": user_id}} , function (err, user) {
+      console.log("data-->"+JSON.stringify(user)+"-------"+user.length);
+      if(user.length){
+
+      }
+      else {
+        console.log("no such user exist");
+      }
+
     });
   }
 
