bfac4a5199b0c69d7e5a58354594aae67a0150d2
rahul.sapkal <rahul.sapkal@neosofttech.com>
2017-07-20 13:09:31 +0530

    added some properties in product.json

--- common/models/product.js
+++ common/models/product.js
@@ -14,7 +14,17 @@ module.exports = function (Product) {
     console.log("-->"+JSON.stringify((context.args.data)));
     next();
   });
-
+  // Product.beforeRemote('updateAttribute', function(context, user, next) {
+  //
+  //   context.args.data.updated_date=new Date();
+  //
+  //   //context.args.data.publisherId = context.req.accessToken.userId;
+  //   // Product.validatesPresenceOf('password', {message: 'Cannot be blank'});
+  //
+  //   console.log(JSON.stringify(user));
+  //   console.log("-->"+JSON.stringify((context.args.data)));
+  //   next();
+  // });
 
 };
 
