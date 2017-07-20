5c7b25a1ecaf98f5bf83644a538059a5ebe4837a
aniket-pracheta <aniket.pracheta@neosofttech.com>
2017-07-20 13:30:45 +0530

    merging code with rahul

--- common/models/image.js
+++ common/models/image.js
@@ -7,7 +7,7 @@ var UserAcc = loopback.getModel("user_account");
 module.exports = function(Image) {
 
   var thumb = require('node-thumbnail').thumb;
-  var imgURL = "http://10.0.100.213:8080";
+  var imgURL = "http://10.0.100.213:8081";
   var containerName = 'image';
   var thumbpath;
   var imgType;
