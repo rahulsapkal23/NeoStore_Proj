e48e40ec432882bfe2ad73c562e9da946bfb9827
Darshana <darshana.patil@wwindia.com>
2017-07-26 19:42:06 +0530

    get cart detail API created

--- common/models/shoppingcart.js
+++ common/models/shoppingcart.js
@@ -2,4 +2,41 @@
 
 module.exports = function(Shoppingcart) {
 
+
+  Shoppingcart.remoteMethod('getCartDetails', {
+    description: "get All CartDetails for a particular user",
+    accepts: [
+      {arg: 'userid', type: 'string', require: true},
+    ],
+    returns: [
+      {arg: 'response', type: 'object'},
+    ],
+    http: {path: '/getCart', verb: 'get'},
+  });
+
+
+
+
+  Shoppingcart.getCartDetails = function (userid,cb){
+
+    Shoppingcart.find({filter:{'where':{'userid':userid}}},function (err, cart) {
+
+      //console.log("get data ---> "+ JSON.stringify(cart));
+      if(err){
+        console.log("error");
+        cb({"message":"Something went wrong"});
+        return;
+      }
+      else{
+        console.log("success");
+        cb(null,cart);
+      }
+
+    });
+
+
+  }
+
+
+
 };
